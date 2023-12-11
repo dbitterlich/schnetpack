417804ea72e67fc231210d4ef32b88efc0fe0731
juliawestermayr <37142621+juliawestermayr@users.noreply.github.com>
2023-12-11 16:17:54 +0100

    Update atomwise.py

--- src/schnetpack/atomistic/atomwise.py
+++ src/schnetpack/atomistic/atomwise.py
@@ -212,6 +212,129 @@ class DipoleMoment(nn.Module):
         inputs[self.dipole_key] = y
         return inputs
 
+class AtomisticDipoleMoment(nn.Module):
+    """
+    Predicts dipole moments from latent partial charges and (optionally) local, atomic dipoles.
+    The latter requires a representation supplying (equivariant) vector features.
+
+    References:
+
+    .. [#painn1] Schütt, Unke, Gastegger.
+       Equivariant message passing for the prediction of tensorial properties and molecular spectra.
+       ICML 2021, http://proceedings.mlr.press/v139/schutt21a.html
+    .. [#irspec] Gastegger, Behler, Marquetand.
+       Machine learning molecular dynamics for the simulation of infrared spectra.
+       Chemical science 8.10 (2017): 6924-6935.
+    .. [#dipole] Veit et al.
+       Predicting molecular dipole moments by combining atomic partial charges and atomic dipoles.
+       The Journal of Chemical Physics 153.2 (2020): 024113.
+    """
+
+    def __init__(
+        self,
+        n_in: int,
+        n_hidden: Optional[Union[int, Sequence[int]]] = None,
+        n_layers: int = 2,
+        activation: Callable = F.silu,
+        predict_magnitude: bool = False,
+        return_charges: bool = False,
+        dipole_key: str = properties.dipole_moment,
+        charges_key: str = properties.partial_charges,
+        correct_charges: bool = True,
+        use_vector_representation: bool = False,
+    ):
+        """
+        Args:
+            n_in: input dimension of representation
+            n_hidden: size of hidden layers.
+                If an integer, same number of node is used for all hidden layers
+                resulting in a rectangular network.
+                If None, the number of neurons is divided by two after each layer
+                starting n_in resulting in a pyramidal network.
+            n_layers: number of layers.
+            activation: activation function
+            predict_magnitude: If true, calculate magnitude of dipole
+            return_charges: If true, return latent partial charges
+            dipole_key: the key under which the dipoles will be stored
+            charges_key: the key under which partial charges will be stored
+            correct_charges: If true, forces the sum of partial charges to be the total
+                charge, if provided, and zero otherwise.
+            use_vector_representation: If true, use vector representation to predict
+                local, atomic dipoles.
+        """
+        super().__init__()
+
+        self.dipole_key = dipole_key
+        self.charges_key = charges_key
+        self.return_charges = return_charges
+        self.model_outputs = [dipole_key]
+        if self.return_charges:
+            self.model_outputs.append(charges_key)
+
+        self.predict_magnitude = predict_magnitude
+        self.use_vector_representation = use_vector_representation
+        self.correct_charges = correct_charges
+
+        if use_vector_representation:
+            self.outnet = spk.nn.build_gated_equivariant_mlp(
+                n_in=n_in,
+                n_out=1,
+                n_hidden=n_hidden,
+                n_layers=n_layers,
+                activation=activation,
+                sactivation=activation,
+            )
+        else:
+            self.outnet = spk.nn.build_mlp(
+                n_in=n_in,
+                n_out=1,
+                n_hidden=n_hidden,
+                n_layers=n_layers,
+                activation=activation,
+            )
+
+    def forward(self, inputs):
+        positions = inputs[properties.R]
+        l0 = inputs["scalar_representation"]
+        natoms = inputs[properties.n_atoms]
+        idx_m = inputs[properties.idx_m]
+        maxm = int(idx_m[-1]) + 1
+
+        if self.use_vector_representation:
+            l1 = inputs["vector_representation"]
+            charges, atomic_dipoles = self.outnet((l0, l1))
+            atomic_dipoles = torch.squeeze(atomic_dipoles, -1)
+        else:
+            charges = self.outnet(l0)
+            atomic_dipoles = 0.0
+
+        if self.correct_charges:
+            sum_charge = snn.scatter_add(charges, idx_m, dim_size=maxm)
+
+            if properties.total_charge in inputs:
+                total_charge = inputs[properties.total_charge][:, None]
+            else:
+                total_charge = torch.zeros_like(sum_charge)
+
+            charge_correction = (total_charge - sum_charge) / natoms.unsqueeze(-1)
+            charge_correction = charge_correction[idx_m]
+            charges = charges + charge_correction
+
+        if self.return_charges:
+            inputs[self.charges_key] = charges
+
+        y = positions * charges
+        if self.use_vector_representation:
+            y = y + atomic_dipoles
+
+        # sum over atoms
+        #y = snn.scatter_add(y, idx_m, dim_size=maxm)
+
+        if self.predict_magnitude:
+            y = torch.norm(y, dim=1, keepdim=False)
+
+        inputs[self.dipole_key] = y
+        return inputs
 
 class Polarizability(nn.Module):
     """
